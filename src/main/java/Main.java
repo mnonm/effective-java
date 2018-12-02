817a42db2263a38c82bebc1d8b7cf200e3fd7253
songseongbin <ppsosqq@gmail.com>
2018-12-02 13:10:03 +0900

    Builder Pattern

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import builder.Calzone;
+import builder.NutritionFacts;
+import builder.NyPizza;
+import builder.Pizza;
+
+public class Main {
+    public static void main(String[] args) {
+        NutritionFacts cocaCola = new NutritionFacts.Builder(240, 8)
+                .calories(100).sodium(35).carbohydrate(27).build();
+
+        NyPizza pizza = new NyPizza.Builder(NyPizza.Size.SMALL)
+                .addTopping(Pizza.Topping.SAUSAGE).addTopping(Pizza.Topping.ONION).build();
+
+        Calzone calzone = new Calzone.Builder()
+                .addTopping(Pizza.Topping.HAM).sauceInside().build();
+    }
+}
