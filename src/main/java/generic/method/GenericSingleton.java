60cc811d0aa2439aca0365be91f3e954d36ed8bf
songseongbin <ppsosqq@gmail.com>
2019-02-05 17:00:40 +0900

    제너릭 메서드

---
+++ src/main/java/generic/method/GenericSingleton.java
@@ -0,0 +1,14 @@
+package generic.method;
+
+
+import java.util.function.UnaryOperator;
+
+public class GenericSingleton {
+
+    private static UnaryOperator<Object> IDENTITY_FN = (t) -> t;
+
+    @SuppressWarnings("unchecked")
+    public static <T> UnaryOperator<T> identityFunction() {
+        return (UnaryOperator<T>) IDENTITY_FN;
+    }
+}
---
+++ src/test/java/generic/method/GenericSingletonTest.java
@@ -0,0 +1,26 @@
+package generic.method;
+
+import org.junit.Test;
+
+import java.util.function.UnaryOperator;
+
+import static org.junit.Assert.*;
+
+public class GenericSingletonTest {
+
+    @Test
+    public void test1() {
+        String[] strings = {"삼베", "대마", "나일론"};
+        UnaryOperator<String> sameString =  GenericSingleton.identityFunction();
+        for(String s : strings) {
+            System.out.println(sameString.apply(s));
+        }
+
+        Number[] numbers = {1, 2.0, 3L};
+        UnaryOperator<Number> sameNumber = GenericSingleton.identityFunction();
+        for(Number n : numbers) {
+            System.out.println(sameNumber.apply(n));
+        }
+    }
+
+}
