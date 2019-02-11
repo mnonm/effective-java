59008dde33ea0c8ccaa8a42b440691eafad64e69
songseongbin <ppsosqq@gmail.com>
2019-02-11 22:02:55 +0900

    enum type - operation test 추가

--- src/test/java/enumType/OperationTest.java
+++ src/test/java/enumType/OperationTest.java
@@ -2,6 +2,9 @@ package enumType;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class OperationTest {
 
     @Test
@@ -11,6 +14,12 @@ public class OperationTest {
         for(Operation op : Operation.values()) {
             System.out.printf("%f %s %f = %f%n", x, op, y, op.apply(x, y));
         }
+    }
+
+    @Test
+    public void test2() {
+        Operation.fromString("-").ifPresent(e -> System.out.println(e.name()));
+        assertThat(        Operation.fromString("-").get(), is(Operation.MINUS));
     }
 
 }
