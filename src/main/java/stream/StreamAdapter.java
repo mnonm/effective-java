7e5845e55e203bced9e788e60c9d96b2ea07376a
songseongbin <ppsosqq@gmail.com>
2019-08-04 18:05:46 +0900

    stream <-> iterator adapter

---
+++ src/main/java/stream/StreamAdapter.java
@@ -0,0 +1,14 @@
+package stream;
+
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public class StreamAdapter {
+    public static <E> Iterable<E> iterableOf(Stream<E> stream) {
+        return stream::iterator;
+    }
+
+    public static <E> Stream<E> streamOf(Iterable<E> iterable) {
+        return StreamSupport.stream(iterable.spliterator(), false);
+    }
+}
