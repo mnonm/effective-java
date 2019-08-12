9293528d2d2d8299a39d93c203068663eeb3ba97
songseongbin <ppsosqq@gmail.com>
2019-08-13 00:49:33 +0900

    가변 데이터 동기화

---
+++ src/main/java/concurrency/VolatileStopThread.java
@@ -0,0 +1,19 @@
+package concurrency;
+
+import java.util.concurrent.TimeUnit;
+
+public class VolatileStopThread {
+    private static volatile boolean stopRequested;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread backgroundThread = new Thread(() -> {
+            int i = 0;
+            while(!stopRequested)
+                i++;
+        });
+        backgroundThread.start();
+
+        TimeUnit.SECONDS.sleep(1);
+        stopRequested = true;
+    }
+}
